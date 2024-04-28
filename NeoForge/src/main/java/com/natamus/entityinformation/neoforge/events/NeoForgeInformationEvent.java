01cba830cb730495c00fb51d156dbf5880304883
Rick South <rick@natamus.com>
2024-04-28 16:04:43 +0200

    Entity Information source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/entityinformation/neoforge/events/NeoForgeInformationEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.entityinformation.neoforge.events;
+
+import com.natamus.entityinformation.cmds.CommandIst;
+import com.natamus.entityinformation.events.InformationEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeInformationEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandIst.register(e.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public static void onEntityDamage(LivingAttackEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (!InformationEvent.onEntityDamage(livingEntity.level(), livingEntity, e.getSource(), e.getAmount())) {
+			e.setCanceled(true);
+		}
+	}
+}
