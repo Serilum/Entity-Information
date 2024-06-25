76083cc5921572ffd4b989463e79189225013c6a
Rick South <rick@natamus.com>
2024-06-26 00:02:31 +0200

    (v3.4) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/entityinformation/neoforge/events/NeoForgeInformationEvent.java
+++ NeoForge/src/main/java/com/natamus/entityinformation/neoforge/events/NeoForgeInformationEvent.java
@@ -3,10 +3,10 @@ package com.natamus.entityinformation.neoforge.events;
 import com.natamus.entityinformation.cmds.CommandIst;
 import com.natamus.entityinformation.events.InformationEvent;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @EventBusSubscriber
 public class NeoForgeInformationEvent {
@@ -16,7 +16,7 @@ public class NeoForgeInformationEvent {
 	}
 
 	@SubscribeEvent
-	public static void onEntityDamage(LivingAttackEvent e) {
+	public static void onEntityDamage(LivingIncomingDamageEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 		if (!InformationEvent.onEntityDamage(livingEntity.level(), livingEntity, e.getSource(), e.getAmount())) {
 			e.setCanceled(true);
