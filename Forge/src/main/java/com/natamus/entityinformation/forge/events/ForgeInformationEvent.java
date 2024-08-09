7203bb01bcd7d6d0333410c1901751cc2a5cb3e8
Rick South <rick@natamus.com>
2024-08-09 08:25:23 +0200

    Entity Information source code for 1.21.1.

---
+++ Forge/src/main/java/com/natamus/entityinformation/forge/events/ForgeInformationEvent.java
@@ -0,0 +1,25 @@
+package com.natamus.entityinformation.forge.events;
+
+import com.natamus.entityinformation.cmds.CommandIst;
+import com.natamus.entityinformation.events.InformationEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeInformationEvent {
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent e) {
+    	CommandIst.register(e.getDispatcher());
+    }
+
+	@SubscribeEvent
+	public void onEntityDamage(LivingAttackEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		if (!InformationEvent.onEntityDamage(livingEntity.level(), livingEntity, e.getSource(), e.getAmount())) {
+			e.setCanceled(true);
+		}
+	}
+}
