6f8d9a76db0d2ff8e86db44e65025f6e5f28e857
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:35:16 +0200

    (v3.8) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/entityinformation/cmds/CommandIst.java
+++ Common/src/main/java/com/natamus/entityinformation/cmds/CommandIst.java
@@ -35,8 +35,8 @@ public class CommandIst {
 		Player player = source.getPlayerOrException();
 		
 		ItemStack informationstick = new ItemStack(Items.STICK, 1);
-		informationstick.setHoverName(Component.literal(ChatFormatting.BLUE + "The Information Stick"));
+		informationstick.setHoverName(Component.translatable("collective.entityinformation.gui.informationstick").withStyle(ChatFormatting.BLUE));
 		player.addItem(informationstick);
-		MessageFunctions.sendMessage(player, "You have been given The Information Stick!", ChatFormatting.BLUE);
+		MessageFunctions.sendTranslatableMessage(player, "collective.entityinformation.message.giveninformationstick", ChatFormatting.BLUE);
 	}
 }
