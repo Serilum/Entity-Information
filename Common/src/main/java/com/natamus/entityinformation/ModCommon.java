efd7d58be653de602327f79fa435f35c41744707
Rick South <rick@natamus.com>
2026-06-17 00:54:04 +0200

    (v3.9) Fixed translations not working when the mod was only installed on the server.

--- Common/src/main/java/com/natamus/entityinformation/ModCommon.java
+++ Common/src/main/java/com/natamus/entityinformation/ModCommon.java
@@ -1,6 +1,9 @@
 package com.natamus.entityinformation;
 
 
+import com.natamus.collective.translations.ServerTranslationPack;
+import com.natamus.entityinformation.util.Reference;
+
 public class ModCommon {
 
 	public static void init() {
@@ -8,6 +11,6 @@ public class ModCommon {
 	}
 
 	private static void load() {
-		
+		ServerTranslationPack.requireClientTranslations(Reference.NAME);
 	}
 }
--- Common/src/main/java/com/natamus/entityinformation/cmds/CommandIst.java
+++ Common/src/main/java/com/natamus/entityinformation/cmds/CommandIst.java
@@ -9,10 +9,12 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.server.permissions.Permissions;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
 
 public class CommandIst {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -38,6 +40,11 @@ public class CommandIst {
 		
 		ItemStack informationstick = new ItemStack(Items.STICK, 1);
 		informationstick.set(DataComponents.CUSTOM_NAME, Component.translatable("collective.entityinformation.gui.informationstick").withStyle(ChatFormatting.BLUE));
+
+		CompoundTag tag = new CompoundTag();
+		tag.putBoolean("informationstick", true);
+		informationstick.set(DataComponents.CUSTOM_DATA, CustomData.of(tag));
+
 		player.addItem(informationstick);
 		MessageFunctions.sendTranslatableMessage(player, "collective.entityinformation.message.giveninformationstick", ChatFormatting.BLUE);
 	}
